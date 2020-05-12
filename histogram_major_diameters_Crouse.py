481a687c92a26e32d2220bc5dcc8ebda48cfbdf9
jaimescrouse <62766581+jaimescrouse@users.noreply.github.com>
2020-05-12 12:12:43 -0500

    Update histogram_major_diameters_Crouse.py

--- histogram_major_diameters_Crouse.py
+++ histogram_major_diameters_Crouse.py
@@ -1,3 +1,36 @@
+'''
+Measurements of each crater’s area, perimeter, major and minor axis to produce dimensionless shape parameters of aspect ratio 
+and isoperimetric circularity. A statistical population to establish quantitative parameters for secondary crater shape in a 
+2D data set was created and analyzed. The method to explicate these diversities include separating various crater shapes into 
+three distinguishable categories: (A) highest continuous rim, (B) depressions interrupted by a convex arc from an overprinted 
+crater (C) multi-basin cases containing complied features from category (A) and (B).
+
+The long-term goal of this research is to provide a catalog of data that establishes the quantitative diversity of secondary 
+impact craters to distinguish them from small primary impacts, small volcanic vents, and collapsed features found on the Moon
+and Mars. 
+
+This routine accepts the applicable data to analyze each class of secondary impact craters laid out in the methodology of this
+project and generates a histogram of each site (proximal and distal) displaying the number of compound secondary impact crater 
+features (combined) which can be described as a Class A feature with 1-3 B features (making it a Class C feature) and compares
+this to the number of secondary impact crater features described as a Class A feature only (single).
+
+Project Title:  DEFINING PARAMETERS FOR SECONDARY IMPACT CRATERS ON THE MOON
+ Abstract: https://ui.adsabs.harvard.edu/abs/2020AAS...23516901C/abstract
+ Publication: American Astronomical Society meeting #235, id. 169.01. 
+              Bulletin of the American Astronomical Society, Vol. 52, No. 1
+ Pub Date: January 2020
+ Bibcode: 2020AAS...23516901C 
+ 
+ Author: Jaime S. Crouse
+ Email: jsazq8@mail.umkc.edu
+ 
+ Co-author: Alison H. Graettinger
+ Acknowledgements: Thank you to my research advisor and co-author, Dr. Alison H. Graettinger and
+ to my graduate student mentor, Kameswara Bharadwaj Mantha (AgentM-GEG) for the support.  
+'''
+
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import  ascii
